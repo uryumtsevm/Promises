373ca6af68d86e63e746dfaa8d46225c8c5a5664
Maxim Uryumtsev <uryumtsevm@gmail.com>
2023-03-11 00:56:04 +0700

    new files js

---
+++ app.js
@@ -0,0 +1,42 @@
+const wringOutTime = 500;
+const squattingTime = 200;
+
+function wringOut(count) {
+  return new Promise((resolve, reject) => {
+    if (count > 100) {
+      reject(new Error("Слишком много отжиманий"));
+    }
+    setTimeout(() => {
+      resolve();
+    }, count * wringOutTime);
+  });
+}
+
+function squatting(count) {
+  return new Promise((resolve, reject) => {
+    if (count > 50) {
+      reject(new Error("Слишком много приседаний"));
+    }
+    setTimeout(() => {
+      resolve();
+    }, count * squattingTime);
+  });
+}
+
+async function myTraining() {
+  try {
+    console.log("Начинаем отжимания");
+    await wringOut(10);
+    console.log("Отжался 10 раз");
+    await squattingTime(20);
+    console.log("Присел 20 раз");
+    return true;
+  } catch (err) {
+    console.log(err.toString());
+    return false;
+  }
+}
+
+myTraining().then((result) => {
+  console.log(result);
+});
